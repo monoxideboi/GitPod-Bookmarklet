525d6474a93fcbcf7def76190d9172b5e1870d73
archerymaster12354 <38110094+archerymaster12354@users.noreply.github.com>
2019-10-11 10:51:22 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,4 @@
+// RAW CODE
 if (window.location.href.indexOf("https://github.com") > -1) {
 window.location.replace(
           "https://gitpod.io/#"+window.location.href
@@ -6,3 +7,6 @@ window.location.replace(
 else {
   alert ("This is not a Github Website. Try find a repository with https://github.com");
 }
+
+// Copy Paste String
+Javascript: if (window.location.href.indexOf("https://github.com") > -1) {window.location.replace("https://gitpod.io/#"+window.location.href);}else {alert ("This is not a Github Website. Try find a repository with https://github.com");}
